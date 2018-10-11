43a37aca07fa137e2656bc22e60491cdedf08ad6
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-11 15:16:19 +0900

    Spring JDBC

--- 201800001-exSpringDI/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
+++ 201800001-exSpringDI/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
@@ -1,10 +1,36 @@
 package kr.ac.dit.persistence;
+import java.sql.*;
+
 import org.springframework.stereotype.Repository;
+
 import kr.ac.dit.domain.StudentVO;
+
 @Repository
+
 public class StudentDAOImpl implements StudentDAO {
-	@Override
-	public void insertStudent(StudentVO studentVO) throws Exception {
-		System.out.println("Spring DI pattern~!");
+
+@Override
+
+public void insertStudent(StudentVO studentVO) throws Exception {
+
+String no = studentVO.getNo();
+
+String name = studentVO.getName();
+
+Class.forName("oracle.jdbc.driver.OracleDriver");
+
+Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "201800001", "201800001");
+
+PreparedStatement pstmt = conn.prepareStatement("insert into student values(?,?)");
+
+pstmt.setString(1, no);
+
+pstmt.setString(2, name);
+
+pstmt.executeUpdate();
+
+pstmt.close();
+
+conn.close();
 	}
 }
